5f6fcecdb4b9ba218a97d50935a258da0ad28d27
feliperieder <67979677+feliperieder@users.noreply.github.com>
2023-10-19 21:49:26 -0300

    Add files via upload

--- TrabalhoGrauA/TrabalhoAblum.py
+++ TrabalhoGrauA/TrabalhoAblum.py
@@ -78,11 +78,11 @@ class Pagina:
             print('Times do Brasileirão')
 
 class Usuario:
-    def __init__(self, nomeDeUsuario, senha) -> None:
+    def __init__(self, nomeDeUsuario, senha,colecao = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] ) -> None:
         self.__nomeDeUsuario = nomeDeUsuario
         self.__senha = senha
         self.album = Album()
-        self.colecao = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        self.colecao = colecao
 
         self.novasFiguras()
 
@@ -140,7 +140,11 @@ class Usuario:
         for nro in range(len(self.colecao)):
             if self.possuiFigurinhaNaColecao(nro):
                 print('Você possui', self.colecao[nro], 'figurinhas', nro+1)
-
+                
+    def trocarFigurinha(self, nro):
+            if self.possuiFigurinhaNaColecao(nro):
+                self.colecao[nro] = self.colecao[nro] -1
+                self.novasFiguras()
     def novasFiguras(self):
         for nro in range(len(self.colecao)):
             if self.possuiFigurinhaNaColecao(nro):
@@ -169,27 +173,51 @@ class Troca:
     def __init__(self, nomeProponente, nomeRequerido, figurinhaRequerida, figurinhaDisponivel) -> None:
         self.__nomeProponente = nomeProponente
         self.__nomeRequerio = nomeRequerido
-        self.__figurinhaRequerida = figurinhaRequerida
-        self.__figurinhaDisponivel = figurinhaDisponivel
+        self.__figurinhaRequerida = int(figurinhaRequerida)
+        self.__figurinhaDisponivel = int(figurinhaDisponivel)
         self.__status = False
 
-    def aceitar(self, aceito):
-        pass
+    def aceitar(self):
+        self.__status = True
+        for usuario in usuarios:
+            if usuario.getNomeDeUsuario() == self.__nomeProponente:
+                usuario.trocarFigurinha(self.__figurinhaDisponivel)
+                usuario.adicionarFigurinhaColecao(self.__figurinhaRequerida)
+            if usuario.getNomeDeUsuario() == self.__nomeRequerio:
+                usuario.trocarFigurinha(self.__figurinhaRequerida)
+                usuario.adicionarFigurinhaColecao(self.__figurinhaDisponivel)
 
-    def informarNomeRequerio(self):
+
+    def recusar(self):
+        self.__status = True
+    def informarNomeRequerido(self):
         return self.__nomeRequerio
-    
     def informarNomeProponente(self):
         return self.__nomeProponente
     def figurinhaRequerida(self):
-        return self.__figurinhaRequerida()
+        return self.__figurinhaRequerida
     def figurinhaDisponivel(self):
         return self.__figurinhaDisponivel
+    def status(self):
+        return self.__status
     
 
 usuarios = []
 trocas = []
+usuarios.append(Usuario('felipe','felipe',[0,2,0,2,0,0,0,2,4,0,0,0,0,0,2,0,0,1,0,0]))
+usuarios.append(Usuario('raphael', 'raphael',[0,2,3,1,0,0,0,2,1,0,2,0,0,1,0,0,0,1,0,3]))
+usuarios.append(Usuario('oi','oi'))
+trocas.append(Troca('raphael', 'felipe', 1, 19))
+#with open('.\Usuarios.csv', mode = 'r', encoding='utf-8', newline='') as arquivo:
+#    leitor_csv = csv.reader(arquivo, delimiter=';')
+#    for linha in leitor_csv:
+#        figurinhas = []
+#        for i in range(len(linha)):
+#            if i < 1:
+#                figurinhas.append(linha(i))
 
+
+print(usuarios)
 def verAlbum(conta):
     pagina = 0
     conta.album.paginas[pagina].show()
@@ -235,11 +263,11 @@ def menuGerenciarColecao(conta):
                         print('Figurinha', status+1, 'disponível para troca')
 
                 nro +=1
-            usuarioTroca = int(input('Diga o número do usuário comquem deseja realizar a troca:\n'))
-            trocaDesejada = int(input('Informe o número da figurinha que você deseja trocar'))
-            figurinhaIndesejada = int(input('Qual figurinha você deseja trocar?\n'))
+            usuarioTroca = int(input('Diga o número do usuário comquem deseja realizar a troca:\n'))-1
+            trocaDesejada = int(input('Informe o número da figurinha que você quer para você\n'))-1
+            figurinhaIndesejada = int(input('Qual figurinha você não quer mais?\n'))-1
             if conta.possuiFigurinhaNaColecao(figurinhaIndesejada):
-                trocas.append(Troca(conta.getNomeDeUsuario, usuarios[usuarioTroca].getNomeDeUsuario, trocaDesejada))
+                trocas.append(Troca(conta.getNomeDeUsuario(), usuarios[usuarioTroca].getNomeDeUsuario(), trocaDesejada, figurinhaIndesejada))
             else:
                 print('você não tem essa figurinha na coleção')
             
@@ -247,22 +275,39 @@ def menuGerenciarColecao(conta):
         elif menu == '4':
             print('Revisar solicitações de troca')
             for troca in trocas:
-                if troca.informarNomeRequerido() == conta.getNomeDeUsuario():
-                    print(troca.informarNomeProponente(), 'deseja trocar a carta número', troca.figurinhaRequerida(),'pela carta', troca.figurinhaDisponivel(),'.')
+                if troca.informarNomeRequerido() == conta.getNomeDeUsuario() and not troca.status():
+                    print(troca.informarNomeProponente(), 'deseja trocar a carta número', troca.figurinhaRequerida()+1, 'pela carta', troca.figurinhaDisponivel()+1, '.')
                     aceitar = input('Aceita realizar a troca? (s/n)\n')
-                    teste = False
-                    while not False:
-                        if aceitar == 'n':
-                            print('Você recusou a troca')
-                            teste = True
-                        elif aceitar == 's':
-                            print('Troca feita')
-                            teste = True
-                        else:
-                            print('Opção inválida, tente novamente')
+                    if aceitar == 'n':
+                        print('Você recusou a troca')
+                        troca.recusar()
+                    elif aceitar == 's':
+                        print('Troca feita')
+                        troca.aceitar()
+                    else:
+                        print('Opção inválida, tente novamente')
+                    break  # Sair do loop após a escolha do usuário
+            #for troca in trocas:
+            #    if troca.informarNomeRequerido() == conta.getNomeDeUsuario() and not troca.status():
+            #        print(troca.informarNomeProponente(), 'deseja trocar a carta número', troca.figurinhaRequerida(),'pela carta', troca.figurinhaDisponivel(),'.')
+            #        aceitar = input('Aceita realizar a troca? (s/n)\n')
+            #        teste = False
+            #        while not teste:
+            #            if aceitar == 'n':
+            #                print('Você recusou a troca')
+            #                teste.recusar()
+            #                teste = True
+            #                break
+            #            elif aceitar == 's':
+            #                print('Troca feita')
+            #                troca.aceitar()
+            #                teste = True
+            #                break
+            #            else:
+            #                print('Opção inválida, tente novamente')
                 
-                else:
-                    print('Ninguém deseja trocar com você')
+            #    else:
+            #        print('Ninguém deseja trocar com você')
 
 
         elif menu == '5':
